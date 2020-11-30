ab1db93604070e01c74a454e4de6e8c4a31d76f8
여율 <dbfm2222@gmail.com>
2020-11-30 23:24:16 +0900

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,17 @@
+// <⚠️ DONT DELETE THIS ⚠️>
+import "./styles.css";
+// <⚠️ /DONT DELETE THIS ⚠️>
+const pendingList = [];
+const finishedList = [];
+
+function paintPanding(text) {
+  const li = document.createElement("li");
+  const delBtn = document.createElement("button");
+  delBtn.innerText("❌"); // ✔
+  const span = document.createElement("span");
+  span.innerText(text);
+}
+function savePanding() {}
+
+function paintFinished() {}
+function saveFinished() {}
